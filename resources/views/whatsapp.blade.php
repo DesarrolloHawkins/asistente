c9faf56e1daff697ba1c9fd3912234e48ec635a1
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2024-03-14 12:57:39 +0100

    Mejoras de estilos

--- resources/views/whatsapp.blade.php
+++ resources/views/whatsapp.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Chat Whatsapp</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
 
@@ -249,7 +249,8 @@
         .chat-panel {
         flex-grow: 1; /* Permite que este div ocupe el espacio restante */
         overflow-y: scroll;
-        height: 88%;
+        height: 85%;
+        padding-bottom: 4rem;
         }
 
         .no-gutters {
