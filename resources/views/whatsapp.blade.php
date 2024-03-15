8fbac9e47d7fc0748b163853426e3c50c2508d64
DesarrolloHawkins <desarrollo.hawkins@gmail.com>
2024-03-15 09:21:57 +0100

    estilos

--- resources/views/whatsapp.blade.php
+++ resources/views/whatsapp.blade.php
@@ -300,7 +300,7 @@
                 flex-grow: 1;
                 overflow-y: scroll;
                 height: 100%;
-                padding-bottom: 2rem;
+                padding-bottom: 8rem;
                 background-image: url(https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png);
             }
         }
