3ba62075d8d872fc9cc3577c866776e95c16f2fb
DesarrolloHawkins <122017573+DesarrolloHawkins@users.noreply.github.com>
2024-07-31 12:16:16 +0200

    asd

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -352,12 +352,30 @@ class WhatsappController extends Controller
                     ]);
                   
                 }
-
-                $response = Http::post('https://crmhawkins.com/updateAyudas', [
+                $dataSend = [
                     'id' => $isAutomatico->ayuda_id,
                     'mensaje' => $mensaje,
                     'interpretado' => $reponseChatGPT1
+                ];
+                $curl = curl_init();
+
+                curl_setopt_array($curl, [
+                    CURLOPT_URL => 'https://crmhawkins.com/updateAyudas',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => json_encode($dataSend),
+                    CURLOPT_HTTPHEADER => [
+                        'Content-Type: application/json'
+                    ],
                 ]);
+        
+                $response = curl_exec($curl);
+                curl_close($curl);
 
             }else {
                 $dataRegistrar = [
